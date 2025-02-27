ba4815af7f4622f345ccf6c2f3c23499765db78b
Ulvie Mustafa <UMMustafa23@codingburgas.bg>
2025-02-28 01:53:50 +0200

    Add raylib (Close #3)

--- StudyGrowth/StudyGrowth/src/mainMenu.cpp
+++ StudyGrowth/StudyGrowth/src/mainMenu.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #include "../include/mainMenu.h"
+#include "raylib.h"
 
 using namespace std;
 
 void mainMenu()
 {
-	cout << "Hello World";
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Raylib window");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Hello World!", 300, 200, 30, GREEN);
+
+        EndDrawing();
+    }
 }
