c5fbd308522f900d89d2aad52ba623047ca942e2
Martin Dimarov <MNDiamarov23@codingburgas.bg>
2025-03-04 20:25:09 +0200

    Add raylib

--- StudyGrowth/StudyGrowth/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "../include/mainMenu.h"
-
-using namespace std;
-
-int main()
-{
-	mainMenu();
-}
---
+++ StudyGrowth/premake5.lua
@@ -0,0 +1,50 @@
+workspace "StudyGrowth"
+    architecture "x64"
+    configurations { "Debug", "Release" }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "StudyGrowth"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    location "StudyGrowth"
+
+    targetdir ("./bin/".. outputdir.. "/%{prj.name}")
+    objdir ("./bin-int/".. outputdir.. "/%{prj.name}") 
+
+    files { 
+      "./%{prj.name}/src/**.cpp", 
+      "./%{prj.name}/src/**.h" 
+    }
+
+    includedirs { 
+      "./vendor/raylib/include", 
+      "./%{prj.name}/src" 
+    }
+
+    libdirs { 
+      "./vendor/raylib/lib" 
+    }
+
+    links { 
+      "raylib", 
+      "opengl32", 
+      "gdi32", 
+      "user32", 
+      "shell32", 
+      "winmm"
+    }
+
+    staticruntime "On"
+    systemversion "latest"
+
+    filter "configurations:Debug"
+      runtime "Debug"
+      staticruntime "off"
+      symbols "On"
+
+    filter "configurations:Release"
+      runtime "Release"
+      staticruntime "off"
+      optimize "On"
